e4d0e3edc6903b855f786e37443a21d43a7d9a3e
danielkalen <d@danielkalen.com>
2019-06-17 19:05:56 -0400

    wip

--- jquery-cache.esm.js
+++ jquery-cache.esm.js
@@ -1,1 +1,3 @@
+var jQuery = require('jquery')
+
 !function(n,r){var $={},$$; $$=function(u){var t=$[u];return t!==r?t:$[u]=n(u)},$$.clear=function(n){$[n]=r},$$.fresh=function(n){return $[n]=r,$$(n)}; module.exports = $$;}(jQuery);
